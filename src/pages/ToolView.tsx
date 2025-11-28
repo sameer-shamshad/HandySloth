328364f39d9429ab529984cd918d79f46bbe55b1
Sameer Shamshad <sameer.shamshad@codeanalytics.us>
2025-11-28 19:27:35 +0500

    Fixed some ui issues.

--- src/pages/ToolView.tsx
+++ src/pages/ToolView.tsx
@@ -16,9 +16,9 @@ const ToolViewPage = () => {
         setIsBookmarked(prev => !prev);
     };
 
-    // if (!tool) {
-    //     return <ToolNotFound />;
-    // }
+    if (!tool) {
+        return <ToolNotFound />;
+    }
 
     return (
         <div className='flex flex-col gap-6'>
@@ -98,7 +98,7 @@ const ToolViewPage = () => {
                         </div>
 
                         <div className='flex items-center gap-1 text-primary-color text-xs ml-auto'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope-fill" viewBox="0 0 16 16">
                             <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
                         </svg>
                             <span>Message</span>

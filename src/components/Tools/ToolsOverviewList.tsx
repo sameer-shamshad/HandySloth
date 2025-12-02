31e8fc0edf9a6aeaa05963be800777ae283c717a
Sameer Shamshad <SameerShamshad.42@gmail.com>
2025-12-02 12:53:13 +0500

    Resolved Top Tools overview responsiveness issues.

--- src/components/Tools/ToolsOverviewList.tsx
+++ src/components/Tools/ToolsOverviewList.tsx
@@ -3,21 +3,21 @@ import type { Tool } from '../../types';
 
 const ToolsOverviewList = ({ tools = [], toolsLabel = "" }: { tools: Tool[], toolsLabel: string }) => {
     return (
-        <div className='w-full grid grid-rows-7 sm:[&>*:nth-child(2)]:border-t'>
-            <h4 className='w-min lg:w-max h-min px-4 text-wrap lg:px-4 lg:text-[14px] 2xl:text-[16px] justify-self-center sm:justify-self-start
+        <div className='w-full grid grid-rows-7'>
+            <h4 className='w-min lg:w-max h-min px-4 text-wrap text-xs sm:text-sm lg:px-4 lg:text-lg 2xl:text-[16px] justify-self-center sm:justify-self-start xl:mx-10 2xl:mx-30
                 leading-none font-extrabold uppercase bg-main-color text-black-color text-center py-2 rounded-lg my-auto'>
                 {toolsLabel}
             </h4>
 
             {
                 tools?.length > 0 && tools.map((tool, index) => (
-                        <div key={tool._id} className='flex items-center gap-3 py-3 border-b border-border-color'>
-                            <span className='text-xs font-semibold text-primary-color px-2 py-1'>{index + 1}</span>
-                            <div className='flex flex-col items-start gap-1 sm:flex-row sm:items-center sm:gap-4 xl:ml-6 xl:gap-5'>
-                                <img src={tool.logo} alt={tool.name} className='h-6 w-6 md:h-8 md:w-8 lg:h-9 lg:w-9 rounded-full object-cover' />
-                                <span className='text-xs font-bold text-primary-color text-wrap mt-1'>{tool.name}</span>
-                            </div>
+                    <div key={tool._id} className='flex items-center gap-3 py-3 border-t border-border-color first:border-t-0 md:last:border-b xl:px-10 2xl:px-30'>
+                        <span className='text-xs font-semibold text-primary-color px-2 py-1'>{index + 1}</span>
+                        <div className='flex flex-col items-start gap-1 sm:flex-row sm:items-center sm:gap-4 xl:ml-3 xl:gap-5'>
+                            <img src={tool.logo} alt={tool.name} className='h-6 w-6 md:h-8 md:w-8 lg:h-9 lg:w-9 rounded-full object-cover' />
+                            <span className='text-xs font-bold text-primary-color text-wrap mt-1'>{tool.name}</span>
                         </div>
+                    </div>
                 ))
             }
         </div>
